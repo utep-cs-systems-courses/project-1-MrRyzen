1a46968e2bc06dc159daab29b62e494be439c8b4
Cristobal Medina <cristobalmd01@gmail.com>
2020-06-25 16:23:44 -0600

    fixed small issue and added two more mehtods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,22 +2,33 @@
 #include "tokenizer.h"
 
 int space_char(char c) {
-  if(c == ' ' || c == '\t')
+  if(c == ' ' || c == '\t' || c == 0)
     return 1;
   return 0;
+}
+
+int non_space_char(char c) {
+  if(c != ' ' && c != '\t' && c != 0)
+    return 1;
+  return 0;
+}
+
+char *word_start(char *str) {
+  int i = 0;
+  while(space_char(str[i]))
+    i++;
+  return &str[i];
 }
 
 int main() {
   char str[100];
 
   printf("> ");
-  gets(str);
+  fgets(str, 101, stdin);
+  char *c = *word_start(str);
+  printf(c);
 
-  printf("\n You Entered: ");
-  puts(str);
-
-  
+  printf("You Entered: %s\n", str);
 
   return 0;
-
 }
